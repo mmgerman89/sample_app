04fa01a0a798b23cb75c6f9269292c4608751a56
German <mmgerman89@gmail.com>
2013-08-18 00:14:37 -0300

    Mejora spec

--- spec/requests/paginas_estaticas_spec.rb
+++ spec/requests/paginas_estaticas_spec.rb
@@ -4,11 +4,18 @@ describe "Paginas Estaticas" do
   let(:titulo_base) { "Tamos" }
   subject { page }
   
+  shared_examples_for "todas las paginas estaticas" do
+    it { should have_selector 'h1',    text: contenido  }
+    it { should have_selector 'title', text: titulo_completo(titulo_pagina) }
+  end
+  
   describe "pagina de inicio" do
     before { visit root_path }
     
-    it { should have_selector 'h1', text: 'Bienvenido' }
-    it { should have_selector 'title', text: titulo_completo('') }
+    let(:contenido) { 'Bienvenido' }
+    let(:titulo_pagina) { '' }
+    
+    it_should_behave_like "todas las paginas estaticas"
     it { should_not have_selector 'title', text: '| Inicio' }
     
   end
@@ -16,24 +23,30 @@ describe "Paginas Estaticas" do
   describe "pagina de ayuda" do
     before { visit ayuda_path }
     
-    it { should have_selector 'h1', text: 'Ayuda' }
-    it { should have_selector 'title', text: titulo_completo('Ayuda') }
+    let(:contenido) { 'Ayuda' }
+    let(:titulo_pagina) { 'Ayuda' }
+    
+    it_should_behave_like "todas las paginas estaticas"
 
   end
   
   describe "pagina acerca de" do
     before { visit acerca_path }
     
-    it { should have_selector 'h1', text: 'Acerca de Tamos' }
-    it { should have_selector 'title', text: titulo_completo('Acerca') }
+    let(:contenido) { 'Acerca de Tamos' }
+    let(:titulo_pagina) { 'Acerca' }
+    
+    it_should_behave_like "todas las paginas estaticas"
 
   end
   
   describe "pagina de contacto" do
     before { visit contacto_path }
     
-    it { should have_selector 'h1', text: 'Contacto' }
-    it { should have_selector 'title', text: titulo_completo('Contacto') }
+    let(:contenido) { 'Contacto' }
+    let(:titulo_pagina) { 'Contacto' }
+    
+    it_should_behave_like "todas las paginas estaticas"
     
   end
 end
